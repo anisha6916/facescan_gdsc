95ef73a39e96f1dff41e3f61ed63682b1adcb8a3
anisha6916 <81490992+anisha6916@users.noreply.github.com>
2024-06-03 17:51:52 -0400

    loading dataset

--- dataset_testing.py
+++ dataset_testing.py
@@ -1,1 +1,33 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import os
+from PIL import Image, ImageOps
+from sklearn.model_selection import train_test_split
 
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+
+
+images = []
+ages = []
+genders = []
+
+for i in os.listdir('../input/utkface-new/crop_part1/')[0:8000]:
+    split = i.split('_')
+    ages.append(int(split[0]))
+    genders.append(int(split[1]))
+    images.append(Image.open('../input/utkface-new/crop_part1/' + i))
+
+images = pd.Series(list(images), name = 'Images')
+ages = pd.Series(list(ages), name = 'Ages')
+genders = pd.Series(list(genders), name = 'Genders')
+
+df = pd.concat([images, ages, genders], axis=1)
+df
+
+display(df['Images'][0])
+print(df['Ages'][0], df['Genders'][0])
